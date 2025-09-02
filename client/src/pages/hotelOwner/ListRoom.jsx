1b9f598dbc2af0a7aecfb72a3bd63c9c1b1adabd
Your Name <you@example.com>
2025-09-02 11:48:53 -0600

    add new listing

--- client/src/pages/hotelOwner/ListRoom.jsx
+++ client/src/pages/hotelOwner/ListRoom.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets, roomsDummyData } from '../../assets/assets'
+import Title from '../../components/Title'
 
 const ListRoom = () => {
+
+    const [rooms, setRooms] = useState(roomsDummyData)
   return (
     <div>
-      
+      <Title align='left' font='outfit' title='Room Listings' subTitle='Review, update, and organize all listed rooms. Maintain accurate details to ensure guests enjoy a seamless booking experience.' />
+      <p className='text-gray-500 mt-8'>All Rooms</p>
+      <div className='bg-white w-full border border-gray-300 text-gray-600'></div>
     </div>
   )
 }
